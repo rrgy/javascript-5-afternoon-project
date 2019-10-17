57eb2816519f5ac8d7f2ad7b085d078ffbe703e8
Ryan Morgan <rrgy80@gmail.com>
2019-10-17 15:34:44 -0600

    finished

--- closures.js
+++ closures.js
@@ -11,19 +11,19 @@
 // Do not edit the code below.
 function outer() {
   var name = 'Tyler';
-  return function() {
+  return function () {
     return 'The original name was ' + name;
   };
 }
 // Do not edit the code above.
-  
+
 /* 
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
+// Code Here
+const inner = outer()
 
 
 //Once you do that, invoke inner.
@@ -52,6 +52,8 @@ function callFriend(name) {
 */
 
 //Code Here
+const callJake = callFriend('Jake')
+callJake(435 - 555 - 9248)
 
 
 
@@ -62,15 +64,22 @@ function callFriend(name) {
 */
 
 //Code Here
+function makeCounter() {
+  let counter = 0
+  function addOne() {
+    return counter += 1
+  }
+  return addOne
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 
 
@@ -89,7 +98,12 @@ function counterFactory(value) {
   // Code here.
 
   return {
-
+    inc: function () {
+      return value += 1
+    },
+    dec: function () {
+      return value -= 1
+    }
   };
 }
 
@@ -109,13 +123,15 @@ counter = counterFactory(10);
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
-function motivation( firstname, lastname ) {
+function motivation(firstname, lastname) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  let message = function () {
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -129,14 +145,14 @@ var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up B
   Invoke this by calling module.publicMethod(); outside the module scope
 */
 
-var module = (function() {
+var module = (function () {
   var person = {
     name: "phillip",
     age: 29,
     location: "Utah"
   };
 
-  function privateMethod(){
+  function privateMethod() {
     return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
   }
 
@@ -144,8 +160,14 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function () {
+      return privateMethod()
+    }
+
   };
 })();
+
+module.publicMethod()
 
 
 
@@ -163,13 +185,19 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function (num) {
+      return secret += num
+    },
+    takeAwayFromSecret: function (num) {
+      return secret -= num
+    }
   };
 }
 
 
 
 ////////// PROBLEM 8 //////////
-  
+
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
@@ -187,10 +215,13 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
+  for (let i = 0; i <= 5; i++) {
+    setTimeout(function () {
+
       console.log(i);
     }, i * 1000);
+
   }
+
 }
 timeOutCounter();
